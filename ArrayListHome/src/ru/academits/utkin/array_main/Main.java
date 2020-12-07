6415186514d15517739f5de6aec95cb131ebb5ea
utkinDE <utkinde@gmail.com>
2020-12-07 23:36:13 +0700

    ArrayListHome v.3.1

--- ArrayListHome/src/ru/academits/utkin/array_main/Main.java
+++ ArrayListHome/src/ru/academits/utkin/array_main/Main.java
@@ -8,27 +8,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String fileName = "input.txt";
-
         try {
-            System.out.println(getStringsFromFile(fileName));
+            String filePath = "ArrayListHome/src/ru/academits/utkin/array_main/input.txt";
+            System.out.println(getStringsFromFile(filePath));
         } catch (FileNotFoundException exception) {
             System.out.println("Файл не найден!");
         }
 
         ArrayList<Integer> numbersList = new ArrayList<>(Arrays.asList(2, 3, 8, 11, 3, 45, 6, 0));
-
-        oddNumbersList(numbersList);
-
+        makeOddNumbersList(numbersList);
         System.out.println("Список без чётных элементов: " + numbersList);
 
         ArrayList<Integer> numbersListWithClones = new ArrayList<>(Arrays.asList(1, 5, 2, 1, 3, 5));
-
         System.out.println("Список без повторяющихся элементов: " + getListWithoutClones(numbersListWithClones));
     }
 
     public static ArrayList<String> getStringsFromFile(String fileName) throws FileNotFoundException {
-        Scanner scanner = new Scanner(new FileInputStream("ArrayListHome/src/ru/academits/utkin/array_main/" + fileName));
+        Scanner scanner = new Scanner(new FileInputStream(fileName));
 
         ArrayList<String> list = new ArrayList<>();
 
@@ -39,7 +35,7 @@ public class Main {
         return list;
     }
 
-    public static void oddNumbersList(ArrayList<Integer> numbers) {
+    public static void makeOddNumbersList(ArrayList<Integer> numbers) {
         for (int i = 0; i < numbers.size(); ++i) {
             if (numbers.get(i) % 2 == 0) {
                 numbers.remove(i);
